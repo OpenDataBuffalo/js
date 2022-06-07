196bf437b87884ccc0b611089940bd5613532eec
Matthew Austin <97620350+opendatamatt@users.noreply.github.com>
2022-06-07 08:56:50 -0400

    Create map_apis.js

---
+++ map_apis.js
@@ -0,0 +1,14 @@
+<script>
+
+//api endpoints for common maps on the Open Data Buffalo portal
+
+var map_neighborhood = "https://data.buffalony.gov/resource/ekfg-mtu8.geojson";
+var map_tracts_2020 = "https://data.buffalony.gov/resource/9f79-wnbd.geojson";
+var map_groups_2020 = "https://data.buffalony.gov/resource/ke5a-pfi5.geojson";
+var map_blocks_2020 = "https://data.buffalony.gov/resource/4j7b-5a5n.geojson";
+var map_council_districts = "https://data.buffalony.gov/resource/u5mx-ugvy.geojson";
+var map_police_districts = "https://data.buffalony.gov/resource/tmq4-c6ja.geojson";
+var map_police_sectors = "https://data.buffalony.gov/resource/2w4j-a2t9.geojson";
+var map_police_cameras = "https://data.buffalony.gov/resource/gpj7-v6rr.geojson";
+
+</script>
